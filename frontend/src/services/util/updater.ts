528ef208911739fbf91b486b6c81b2e38cf5a08f
Zack-Xb <zack.xuereb.21@um.edu.mt>
2024-02-20 12:49:59 +0100

    adds updater and deep link handlers in frontend

---
+++ frontend/src/services/util/updater.ts
@@ -0,0 +1,8 @@
+import { check } from "@tauri-apps/plugin-updater";
+import { relaunch } from "@tauri-apps/plugin-process";
+
+const update = await check();
+if (update?.available) {
+  await update.downloadAndInstall();
+  await relaunch();
+}
