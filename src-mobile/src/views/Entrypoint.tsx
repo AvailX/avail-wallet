2fa63ed076ba0d0c79cb72a3abda5dd4bb07d7e0
kalio007 <kalioprince1@gmail.com>
2024-05-29 11:59:57 +0100

    navigation

--- src-mobile/src/views/Entrypoint.tsx
+++ src-mobile/src/views/Entrypoint.tsx
@@ -1,15 +1,35 @@
-import * as React from 'react';
-import * as mui from '@mui/material';
-import logo from '../assets/logos/avail-black-icon.svg';
+import * as React from "react";
+import * as mui from "@mui/material";
+import logo from "../assets/logos/avail-black-icon.svg";
+import { useNavigate } from "react-router-dom";
 
-function EntryPoint(){
-    return (
-        <mui.Box sx={{
-            display: 'flex', alignItems: 'center', alignContent: 'center', height: '100vh', justifyContent: 'center', bgcolor: '#00FFAA', width: '100vw', overflow: 'hidden',m: 0, p: 0
-        }}>
-	        <img src={logo} style={{ width: '30%', alignSelf: 'center' }} />
-        </mui.Box>
-    )
+function EntryPoint() {
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    setTimeout(() => {
+      //   navigate("/dashboard");
+      // commenting to test the wallet connect
+      navigate("/wallet-connect");
+    }, 2000);
+  }, []);
+  return (
+    <mui.Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        alignContent: "center",
+        height: "100vh",
+        justifyContent: "center",
+        bgcolor: "#00FFAA",
+        width: "100vw",
+        overflow: "hidden",
+        m: 0,
+        p: 0,
+      }}
+    >
+      <img src={logo} style={{ width: "30%", alignSelf: "center" }} />
+    </mui.Box>
+  );
 }
 
 export default EntryPoint;
