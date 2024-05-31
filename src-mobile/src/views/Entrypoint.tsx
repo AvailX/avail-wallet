12baa7c2beb2ea98fd46e2f09e8f091f25b128de
kalio007 <kalioprince1@gmail.com>
2024-05-31 23:58:37 +0100

    changed entrypoint navigate

--- src-mobile/src/views/Entrypoint.tsx
+++ src-mobile/src/views/Entrypoint.tsx
@@ -7,7 +7,7 @@ function EntryPoint() {
   const navigate = useNavigate();
   React.useEffect(() => {
     setTimeout(() => {
-      navigate("/wallet-connect");
+      navigate("/dashboard");
     }, 2000);
   }, []);
   return (
