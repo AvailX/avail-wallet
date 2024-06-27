1e8d5a157928bd59e5e43cfa6f5eeadd0908831c
dev-blc <genesis.dev.blc@gmail.com>
2024-06-27 15:39:10 +0530

    fixes indent

--- src/views-desktop/quests/quests.tsx
+++ src/views-desktop/quests/quests.tsx
@@ -7,31 +7,31 @@ import SideMenu from '../../components/sidebar';
 import QuestBox from '../../components/quests/quest';
 import TaskDrawer from '../../components/quests/tasks_drawer';
 import ConfirmDialog from "../../components/quests/CreateQuests/ConfirmDialog";
-import { AddQuestTasksProps, Campaign} from "../../../src/types/quests/quest_types";
+import { AddQuestTasksProps, Campaign } from "../../../src/types/quests/quest_types";
 import ScanReAuthDialog from '../../components/dialogs/reauth';
 // Types
-import {type CampaignDetailPageProps} from '../../types/quests/quest_types';
-import {type Quest} from '../../types/quests/quest_types';
+import { type CampaignDetailPageProps } from '../../types/quests/quest_types';
+import { type Quest } from '../../types/quests/quest_types';
 
 // Images
 import verified from '../../assets/icons/verified.svg';
 
 // Typography
-import {BodyText500} from '../../components/typography/typography';
+import { BodyText500 } from '../../components/typography/typography';
 
 // Services
-import {deleteCampaign, isQuestCompleted} from '../../services/quests/quests';
+import { deleteCampaign, isQuestCompleted } from '../../services/quests/quests';
 
 // Hooks
-import {useLocation, useNavigate} from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 // Alerts
-import {SuccessAlert, ErrorAlert} from '../../components/snackbars/alerts';
+import { SuccessAlert, ErrorAlert } from '../../components/snackbars/alerts';
 import { get } from 'http';
 import { get_address } from '../../services/storage/persistent';
 
 const Quests: React.FC = () => {
-	const {campaign, quests} = useLocation().state as CampaignDetailPageProps;
+	const { campaign, quests } = useLocation().state as CampaignDetailPageProps;
 	const [quest, setQuest] = React.useState<Quest>();
 	const [openTasks, setOpenTasks] = React.useState(false);
 	const [questCompleted, setQuestCompleted] = React.useState(false);
@@ -82,53 +82,87 @@ const Quests: React.FC = () => {
 			console.log(err);
 		});
 	};
-	const handleDeleteConfirmation = () =>{
+	const handleDeleteConfirmation = () => {
 		setOpenDialog(true);
 	};
 	return (
 		<Layout>
-			<ErrorAlert errorAlert={error} setErrorAlert={setError} message={message}/>
-			<SuccessAlert successAlert={success} setSuccessAlert={setSuccess} message={message}/>
-			<SideMenu/>
-			<TaskDrawer open={openTasks} onClose={() => {setOpenTasks(false)}} quest={quest} />
-			<mui.Box sx={{ml: md ? '5%' : '7%', display: 'flex', flexDirection: 'column', width: md ? '95%' : '93%'}}>
-				<mui.Box sx={{background: `url(${campaign.bg_image})`, color: campaign.color, backgroundPosition: lgxl ? 'center' : 'bottom', height: lgxl ? '380px' : '320px', backgroundSize: 'cover'}}>
-					<mui.Box sx={{borderRadius: '100%', border: '1px solid #696969', p: 1.5, width: '200px', mt: lgxl ? '10%' : lg ? '8%' : lgsx ? '10%' : mdlg ? '10%' : md ? '13%' : mdsx ? '14%' : '17%', ml: '5%' }}>
+			<ErrorAlert errorAlert={error} setErrorAlert={setError} message={message} />
+			<SuccessAlert successAlert={success} setSuccessAlert={setSuccess} message={message} />
+			<SideMenu />
+			<TaskDrawer open={openTasks} onClose={() => { setOpenTasks(false) }} quest={quest} />
+			<mui.Box sx={{ ml: md ? '5%' : '7%', display: 'flex', flexDirection: 'column', width: md ? '95%' : '93%' }}>
+				<mui.Box sx={{ background: `url(${campaign.bg_image})`, color: campaign.color, backgroundPosition: lgxl ? 'center' : 'bottom', height: lgxl ? '380px' : '320px', backgroundSize: 'cover' }}>
+					<mui.Box sx={{ borderRadius: '100%', border: '1px solid #696969', p: 1.5, width: '200px', mt: lgxl ? '10%' : lg ? '8%' : lgsx ? '10%' : mdlg ? '10%' : md ? '13%' : mdsx ? '14%' : '17%', ml: '5%' }}>
 						<mui.Box
 							component='img'
 							src={campaign.profile_image}
-							sx={{borderRadius: 0, maxWidth: '100%'}}
+							sx={{ borderRadius: 0, maxWidth: '100%' }}
 						/>
 					</mui.Box>
 					<mui.Box
 						component='img'
 						src={verified}
-						sx={{borderRadius: 0, ml: lgxl ? '11%' : lg ? '13%' : lgsx ? '15%' : mdlg ? '17%' : md ? '20%' : '23%', mt: lg ? '-8%' : lgsx ? '-10%' : '-12%'}}
+						sx={{ borderRadius: 0, ml: lgxl ? '11%' : lg ? '13%' : lgsx ? '15%' : mdlg ? '17%' : md ? '20%' : '23%', mt: lg ? '-8%' : lgsx ? '-10%' : '-12%' }}
 					/>
-					
+
 				</mui.Box>
-				<mui.Box sx={{ml: '2%', mt: '5%'}}>
+				<mui.Box sx={{ ml: '2%', mt: '5%' }}>
 					<mui.Typography variant='h3' color='#FFF'>{campaign.title}</mui.Typography>
 					<BodyText500 color='#A3A3A3'>{campaign.inner_description}</BodyText500>
+					<mui.Box >
+						{campaign.owner === ownerAddr && (
+							// <mui.Button variant="contained" color="primary" onClick={handleAddQuest}>
+							// 	Add Quest
+							// </mui.Button>
+							<mui.Box
+								sx={{
+									backgroundColor: '#fff',
+									height: '40px',
+									width: '150px',
+									mr: '2%',
+									borderRadius: '8px',
+									alignContent: 'center',
+									textAlign: 'center',
+									marginRight: '20px',
+									cursor: 'pointer',
+								}}
+								onClick={handleAddQuest}
+							>
+								<mui.Typography fontSize={'15px'} color="#000" variant="h6">
+									Add Quest
+								</mui.Typography>
+							</mui.Box>
+						)}
+					</mui.Box>
 					<mui.Box>
 						{campaign.owner === ownerAddr && (
-						<mui.Button variant="contained" color="primary" onClick={handleAddQuest}>
-							Add Quest
-						</mui.Button>
+							<mui.Box
+								sx={{
+									backgroundColor: '#fff',
+									height: '40px',
+									width: '150px',
+									mr: '2%',
+									borderRadius: '8px',
+									alignContent: 'center',
+									textAlign: 'center',
+									marginRight: '20px',
+									cursor: 'pointer',
+									marginTop: '10px',
+								}}
+								onClick={handleDeleteConfirmation}
+							>
+								<mui.Typography fontSize={'15px'} color="#000" variant="h6">
+									Delete Campaign
+								</mui.Typography>
+							</mui.Box>
 						)}
-					</mui.Box>  
-					<mui.Box>
-						{campaign.owner === ownerAddr && (
-						<mui.Button variant="contained" color="primary" onClick={handleDeleteConfirmation}>
-						Delete Campaign
-						</mui.Button>
-						)}
-					</mui.Box>  
+					</mui.Box>
 				</mui.Box>
-				<mui.Divider sx={{width: '100%', height: '1px', bgcolor: '#00FFAA', mt: '3%'}} orientation='horizontal'/>
-				<mui.Grid container spacing={2} sx={{marginTop: '20px', alignItems: 'center', mb: '5%', paddingLeft: '2%', bgcolor: '#111111', alignSelf: 'center', width: '100%', justifyContent: 'space-around'}}>
+				<mui.Divider sx={{ width: '100%', height: '1px', bgcolor: '#00FFAA', mt: '3%' }} orientation='horizontal' />
+				<mui.Grid container spacing={2} sx={{ marginTop: '20px', alignItems: 'center', mb: '5%', paddingLeft: '2%', bgcolor: '#111111', alignSelf: 'center', width: '100%', justifyContent: 'space-around' }}>
 					{quests.map(quest => (
-						<QuestBox key={quest.id} quest={quest} openTasks={openTasks} setOpenTasks={setOpenTasks} setQuest={setQuest}/>
+						<QuestBox key={quest.id} quest={quest} openTasks={openTasks} setOpenTasks={setOpenTasks} setQuest={setQuest} />
 					))}
 				</mui.Grid>
 				<ConfirmDialog
@@ -139,11 +173,11 @@ const Quests: React.FC = () => {
 					content="Are you sure you want to delete this campaign? This action is irreversible."
 				/>
 				{/* ReAuth Dialog */}
-			<ScanReAuthDialog isOpen={reAuthDialogOpen} onRequestClose={() => {
-				setReAuthDialogOpen(false);
-			}} />
+				<ScanReAuthDialog isOpen={reAuthDialogOpen} onRequestClose={() => {
+					setReAuthDialogOpen(false);
+				}} />
 			</mui.Box>
-		</Layout>
+		</Layout >
 	);
 };
 
