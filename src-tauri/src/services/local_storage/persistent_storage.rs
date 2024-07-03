339f46e6f5fc43ed06a5f1c9bd165ab7be435d27
dev-blc <genesis.dev.blc@gmail.com>
2024-07-03 19:51:18 +0530

    updates testnet merge inconsistency

--- src-tauri/src/services/local_storage/persistent_storage.rs
+++ src-tauri/src/services/local_storage/persistent_storage.rs
@@ -60,7 +60,7 @@ pub fn initial_user_preferences(
             &"dark",
             &language.to_string_short(),
             // TODO - V2 change default to mainnet
-            &"testnet3",
+            &"testnet",
             &auth_type,
             &username,
             &tag,
@@ -531,12 +531,12 @@ fn test_get_network() {
     let res = get_network().unwrap();
 
     print!("{}", res);
-    assert_eq!(res, "testnet3".to_string());
+    assert_eq!(res, "testnet".to_string());
 }
 
 #[test]
 fn test_get_address() {
-    let address = get_address::<Testnet3>().unwrap();
+    let address = get_address::<TestnetV0>().unwrap();
 
     print!("{}", address);
 }
@@ -581,7 +581,7 @@ async fn test_timestamp_to_blockheight() {
 
     let client = tauri_plugin_http::reqwest::Client::new();
     let query = format!(
-        "https://aleo-testnet3.obscura.network/api/{}/blocks/timestamps?start={}&end={}
+        "https://aleo-testnetbeta.obscura.network/api/{}/blocks/timestamps?start={}&end={}
     ",
         obscura_api_key,
         timestamp.timestamp(),
@@ -590,7 +590,7 @@ async fn test_timestamp_to_blockheight() {
 
     let response = client.get(query).send().await.unwrap();
     println!("{:?}", response);
-    let response: Vec<Block<Testnet3>> = response.json().await.unwrap();
+    let response: Vec<Block<TestnetV0>> = response.json().await.unwrap();
     let latest_height = response[0].height();
 
     println!("Latest height: {}", latest_height);
