e76663a958efd28ed0cf9515ebd12647e2fa2143
kalio007 <kalioprince1@gmail.com>
2024-07-18 16:14:39 +0100

    changed the search tabs

--- src-mobile/src/browser/mobile_browser.tsx
+++ src-mobile/src/browser/mobile_browser.tsx
@@ -48,7 +48,7 @@ const Search = styled("div")(({ theme }) => ({
   borderRadius: theme.shape.borderRadius,
   backgroundColor: theme.palette.common.white,
   marginLeft: 0,
-  width: "50%",
+  width: "100%",
 }));
 
 type BrowserProperties = {
@@ -334,7 +334,7 @@ const Browser: React.FC<BrowserProperties> = ({
         </IconButton>
       </Box> */}
       <AppBar position="static" sx={{ bgcolor: "#111111" }}>
-        <Stack direction="row" spacing={1}>
+        <Stack direction="row" spacing={0.5}>
           <IconButton
             edge="start"
             color="inherit"
@@ -353,36 +353,7 @@ const Browser: React.FC<BrowserProperties> = ({
           >
             <RefreshIcon fontSize="small" />
           </IconButton>
-        </Stack>
-        <Toolbar variant="dense">
-          <Search>
-            <Paper
-              component="form"
-              sx={{
-                p: "2px 2px",
-                display: "flex",
-                alignItems: "center",
-                width: "100%",
-              }}
-              onSubmit={handleInputSubmit}
-            >
-              <InputBase
-                sx={{
-                  ml: 0.5,
-                  height: "30px",
-                  flex: 1,
-                  "& input::placeholder": {
-                    fontSize: "14px",
-                  },
-                }}
-                placeholder={t("browser.enter") + " URL"}
-                inputProps={{ "aria-label": "enter url" }}
-                value={inputUrl}
-                onChange={handleInputChange}
-              />
-            </Paper>
-          </Search>
-          <Box sx={{ width: "25%", m: "1%" }}>
+          <Box sx={{ width: "70%" }}>
             <Paper
               component="form"
               sx={{
@@ -412,6 +383,7 @@ const Browser: React.FC<BrowserProperties> = ({
           <Button
             sx={{
               borderRadius: "10px",
+              height: "35px",
               width: "20%",
               bgcolor: "#00FFAA",
               color: "#111111",
@@ -436,7 +408,36 @@ const Browser: React.FC<BrowserProperties> = ({
               ? t("browser.message.success.disconnect")
               : t("browser.connect")}
           </Button>
-        </Toolbar>
+        </Stack>
+        {/* <Toolbar variant="dense"> */}
+        {/* <Search> */}
+        {/* <Paper
+              component="form"
+              sx={{
+                p: "2px 2px",
+                display: "flex",
+                alignItems: "center",
+                width: "100%",
+              }}
+              onSubmit={handleInputSubmit}
+            >
+              <InputBase
+                sx={{
+                  ml: 0.5,
+                  height: "30px",
+                  flex: 1,
+                  "& input::placeholder": {
+                    fontSize: "14px",
+                  },
+                }}
+                placeholder={t("browser.enter") + " URL"}
+                inputProps={{ "aria-label": "enter url" }}
+                value={inputUrl}
+                onChange={handleInputChange}
+              />
+            </Paper> */}
+        {/* </Search> */}
+        {/* </Toolbar> */}
       </AppBar>
       <Box
         sx={{
