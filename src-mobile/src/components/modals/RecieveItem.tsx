c1e9a76b50d07a2b465f22e3c428aace978e3d28
Nwakanma Dominion <118551746+Immadominion@users.noreply.github.com>
2024-07-25 19:23:17 +0100

    INFO: edit to the Receive Item file

--- src-mobile/src/components/modals/RecieveItem.tsx
+++ src-mobile/src/components/modals/RecieveItem.tsx
@@ -1,18 +1,35 @@
 import { Box, Typography } from "@mui/material";
-
 import qrCode from "../../assets/qrcode.svg";
 import copyIcon from "../../assets/copy-icon.svg";
+import { truncateText } from "components/DashboardHeader";
+import { toast } from "react-toastify";
 
 interface IReceiveItemProps {
   header: string;
   walletAddress: string;
   image?: string | undefined;
+  onQrCodeClick: () => void;
 }
 const ReceiveItem: React.FC<IReceiveItemProps> = ({
   header,
   walletAddress,
   image,
+  onQrCodeClick,
 }) => {
+  //This function enables copying wallet address to clipboard
+  const copyAddress = () => {
+    navigator.clipboard
+      .writeText(walletAddress)
+      .then(() => {
+        toast.success("Wallet address copied to clipboard!");
+        console.log("NOTE: <<address has been copied>> ");
+      })
+      .catch(() => {
+        toast.error("Failed to copy wallet address.");
+        console.log("ERROR: <<could not copy address>>");
+      });
+  };
+
   return (
     <>
       <Box
@@ -20,7 +37,7 @@ const ReceiveItem: React.FC<IReceiveItemProps> = ({
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          gap: "5",
+          gap: "5px",
         }}
       >
         {image !== "undefined" ? (
@@ -42,22 +59,30 @@ const ReceiveItem: React.FC<IReceiveItemProps> = ({
         )}
         <Box style={{ marginRight: "auto", marginLeft: "1rem" }}>
           <Typography style={{ color: "#B6B6B6" }}>{header}</Typography>
-          <Typography style={{ color: "#969696" }}>{walletAddress}</Typography>
+          <Typography style={{ color: "#969696" }}>
+            {truncateText(walletAddress, 10)}
+          </Typography>
         </Box>
         <Box
           style={{
             display: "flex",
             alignItems: "center",
-            gap: "4",
+            gap: "4px",
           }}
         >
           <img
             src={qrCode}
             alt=""
             width={"30rem"}
-            style={{ marginInline: "1rem" }}
+            style={{ marginInline: "1rem", cursor: "pointer"  }}
+            onClick={onQrCodeClick}
           />
-          <img src={copyIcon} alt="" width={"30rem"} />
+          <img
+            src={copyIcon}
+            alt=""
+            width={"30rem"}
+            onClick={() => copyAddress()}
+          />
         </Box>
       </Box>
     </>
