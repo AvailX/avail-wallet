a198cd152c2746eb096189cbbb05987dbd5fa1fc
kalio007 <kalioprince1@gmail.com>
2024-08-26 15:19:34 +0100

    fix: changed layout image

--- src/views-desktop/reusable/layout.tsx
+++ src/views-desktop/reusable/layout.tsx
@@ -1,28 +1,35 @@
-import * as React from 'react';
-import * as mui from '@mui/material';
-import MiniDrawer from '../../components/sidebar';
+import * as React from "react";
+import * as mui from "@mui/material";
+import MiniDrawer from "../../components/sidebar";
+import revampBg from "../../assets/images/backgrounds/revamp_bg1.png";
 
 type LayoutWrapperProperties = {
-	children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 const Layout: React.FC<LayoutWrapperProperties> = ({ children }) => (
-	<div>
-		{/* <MiniDrawer></MiniDrawer> */}
+  <div>
+    {/* <MiniDrawer></MiniDrawer> */}
 
-		<mui.Box sx={{
-			display: 'flex',
-			flexDirection: 'column',
-			// BackgroundColor:"#081424",
-			backgroundColor: '#111111',
-			minWidth: '100%',
-			minHeight: '100vh',
-			margin: 0,
-			padding: 0,
-		}}>
-			{children}
-		</mui.Box>
-	</div>
+    <mui.Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        // BackgroundColor:"#081424",
+        backgroundColor: "#111111",
+        minWidth: "100%",
+        minHeight: "100vh",
+        margin: 0,
+        padding: 0,
+        backgroundImage: `url(${revampBg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "bottom center",
+        backgroundSize: "100% auto",
+      }}
+    >
+      {children}
+    </mui.Box>
+  </div>
 );
 
 export default Layout;
